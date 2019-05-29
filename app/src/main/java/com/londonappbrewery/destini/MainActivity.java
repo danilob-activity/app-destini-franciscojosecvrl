2aa637c37b90829d4cbf63ca093134fedc1417ef
Marcos Filho <madlnf@hotmail.com>
2019-05-29 11:58:02 -0300

    Aula 29/05

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -17,6 +17,22 @@ public class MainActivity extends AppCompatActivity {
     //indice corrente da historia
     private Story mStorySelected;
 
+    Story mT1 = new Story(R.sting.T1_Stroy);
+    Story mT2 = new Story(R.string.T2_Story);
+    Story mT3 = new Story(R.string.T3_Story);
+    Story mT4 = new Story(R.string.T4_End);
+    Story mT5 = new Story(R.string.T5_End);
+    Story mT6 = new Story(R.string.T6_End);
+
+    Answer ansT1_1 = new Answer(R.string.T1_Ans1);
+    Answer ansT1_2 = new Answer(R.string.T1_Ans2);
+    Answer ansT2_2 = new Answer(R.string.T2_Ans1);
+    Answer ansT2_1 = new Answer(R.string.T2_Ans2);
+    Answer ansT3_1 = new Answer(R.string.T3_Ans1);
+    Answer ansT3_2 = new Answer(R.string.T3_Ans2);
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,9 +40,28 @@ public class MainActivity extends AppCompatActivity {
 
 
         //TODO: Faça o link do layout com a activity
+        mStoryTextView = findViewById(R.id.storyTextView);
+        mAnswerBottom = findViewById(R.id.buttonBottom);
+        mAnswerTop = findViewById(R.id.buttonTop);
+
 
         //TODO:faça o mapeamento da história
 
+
+        mT1.setAnswerTop(ansT1_1);
+        mT1.setAnswerBottom(ansT1_2);
+        ansT1_1.setChildStory(mT3);
+
+        mT2.setAnswerTop(ansT2_1);
+        mT2.setAnswerBottom(ansT2_2);
+        ansT2_1.setChildStory(mT3);
+
+        mT3.setAnswerTop(ansT3_1);
+        mT3.setAnswerBottom(ansT3_2);
+        ansT3_1.setChildStory(mT6);
+        ansT3_2.setChildStory(mT5);
+
+        
         // TODO: Coloque o evento do click do botão, caso precise colocar a visibilidade no botão invisivel utilize a função
         // do botão setVisibility(View.GONE):
 
